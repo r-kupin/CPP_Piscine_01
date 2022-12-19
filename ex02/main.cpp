c00ce0f9cf57604ffed5609d4b7c8f96bced3b6f
rokupin <rokupin@student.42.fr>
2022-12-19 20:42:14 +0100

    ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -10,3 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+
+int main() {
+	const std::string kString = "HI THIS IS BRAIN";
+	const std::string& kReferenceString = kString;
+	const std::string *ptr_string = &kString;
+
+	std::cout << "string itself: " << kString << std::endl;
+	std::cout << "reference to the string: " << kReferenceString << std::endl;
+	std::cout << "pointer value of the string: " << *ptr_string << std::endl;
+	std::cout << "pointer address of the string: " << ptr_string << std::endl;
+
+	return 0;
+}
