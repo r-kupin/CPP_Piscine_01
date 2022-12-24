6d1dd8d83638de414243990b9d52dabefbba4838
rokupin <rokupin@student.42.fr>
2022-12-24 21:46:42 +0100

    ex05

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,27 @@
 /*   By: rokupin <rokupin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 00:19:44 by rokupin           #+#    #+#             */
-/*   Updated: 2022/12/07 00:19:45 by rokupin          ###   ########.fr       */
+/*                                                    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+#include "Karen.hpp"
+
+int main() {
+	Karen karen;
+
+	std::cout << "=====ERROR=====" << std::endl;
+	karen.complain("ERROR");
+	std::cout << "=====WARNING=====" << std::endl;
+	karen.complain("WARNING");
+	std::cout << "=====INFO=====" << std::endl;
+	karen.complain("INFO");
+	std::cout << "=====DEBUG=====" << std::endl;
+	karen.complain("DEBUG");
+	std::cout << "=====RAMPAGE=====" << std::endl;
+	karen.complain("RAMPAGE");
+
+	return (0);
+}
